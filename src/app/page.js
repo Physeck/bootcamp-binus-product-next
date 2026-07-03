908be2b167745187bfc788fd44ab69bcdf4f9cab
Physeck <justin.tjandra@binus.ac.id>
2026-07-03 19:12:26 +0700

    chore: force dynamic rendering for home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { getProduct } from "./features/products/services/productApi";
 import HomeClientLayout from "@/components/organisms/HomeClientLayout";
 
